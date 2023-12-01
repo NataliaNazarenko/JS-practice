809b01ac64e113220643346aae02ef845d1b6d6e
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-12-01 23:21:00 +0200

    add Lesson-2

--- Lesson-1/index.js
+++ Lesson-1/index.js
@@ -1,59 +1,59 @@
 // Number
 
-const value = '3';
-console.log (typeof value);
-console.log(Number(value));
+// const value = '3';
+// console.log (typeof value);
+// console.log(Number(value));
 
 // перевірка на число
 
-const number = Number('51');
-console.log(Number.isNaN(number));
+// const number = Number('51');
+// console.log(Number.isNaN(number));
 
-const string = Number('hello');
-console.log(Number.isNaN(string));
+// const string = Number('hello');
+// console.log(Number.isNaN(string));
 
 // Number.parseFloat() Number.parseInt()
 
-console.log(Number.parseInt('5ddda'));
-console.log(Number.parseInt('51/ddda457'));
-console.log(Number.parseInt('2.fafr456'));
-console.log(Number.parseInt('bnhsw'));
+// console.log(Number.parseInt('5ddda'));
+// console.log(Number.parseInt('51/ddda457'));
+// console.log(Number.parseInt('2.fafr456'));
+// console.log(Number.parseInt('bnhsw'));
 
 // парсить у строки число з крапкою
 
-console.log(Number.parseFloat('5ddda'));
-console.log(Number.parseFloat('51/ddda457'));
-console.log(Number.parseFloat('2.3fafr456'));
-console.log(Number.parseFloat('bnhsw'));
+// console.log(Number.parseFloat('5ddda'));
+// console.log(Number.parseFloat('51/ddda457'));
+// console.log(Number.parseFloat('2.3fafr456'));
+// console.log(Number.parseFloat('bnhsw'));
 
 // складання з крапкою
 
-console.log(0.1 + 0.2 === 0.3);
-console.log(0.1 + 0.2);
-console.log((0.1 * 10 + 0.2 * 10) / 10);
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2);
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
 
 // Об"єкт Math
 
-console.log(Math.floor(1.7));
-console.log(Math.ceil(3.7));
-console.log(Math.round(1.8));
-console.log(Math.round(1.4));
-console.log(Math.max(10, 50, 40, 3, 5));
-console.log(Math.min(34, 28, -8, 45, 97));
-console.log(Math.pow(2, 4));
-console.log(Math.random());  //0-1
+// console.log(Math.floor(1.7));
+// console.log(Math.ceil(3.7));
+// console.log(Math.round(1.8));
+// console.log(Math.round(1.4));
+// console.log(Math.max(10, 50, 40, 3, 5));
+// console.log(Math.min(34, 28, -8, 45, 97));
+// console.log(Math.pow(2, 4));
+// console.log(Math.random());  //0-1
 // console.log(Math.random() * (max - min) + min);
-console.log(Math.random() * (10 - 1) + 1);  //1-10
+// console.log(Math.random() * (10 - 1) + 1);  //1-10
 
 // String
 
-let result;
-result = 5 + 5 + '5';
-console.log(result);
-console.log(typeof result);
-const num = 55;
-result = `${num} cars`
-console.log(result);
+// let result;
+// result = 5 + 5 + '5';
+// console.log(result);
+// console.log(typeof result);
+// const num = 55;
+// result = `${num} cars`
+// console.log(result);
 
 // методи строк
 
@@ -63,60 +63,60 @@ console.log(result);
 // toLowerCase()
 // toUpperCase()
 
-const message = 'Hello, I am Natalia';
-console.log(message.length);
-console.log(message.toLowerCase());
-console.log(message.toUpperCase());
-console.log(message.indexOf('t'));
-console.log(message.indexOf('cat'));
-console.log(message.includes('hello'));
-console.log(message.includes('Hello'));
+// const message = 'Hello, I am Natalia';
+// console.log(message.length);
+// console.log(message.toLowerCase());
+// console.log(message.toUpperCase());
+// console.log(message.indexOf('t'));
+// console.log(message.indexOf('cat'));
+// console.log(message.includes('hello'));
+// console.log(message.includes('Hello'));
 
 // BigInt
 
-const a = 54789214569586256n;
-console.log(typeof a);
+// const a = 54789214569586256n;
+// console.log(typeof a);
 
 
 // Об"єкти і їх методи
 
-const obj = {
-    name: "Natalia",
-    age: 38, "like cats": true,
-}
+// const obj = {
+//     name: "Natalia",
+//     age: 38, "like cats": true,
+// }
 
-console.log(obj['like cats']);
-delete obj['like cats'];
-console.log(obj);
+// console.log(obj['like cats']);
+// delete obj['like cats'];
+// console.log(obj);
 
-const user = {};
-user.name = 'Mango';
-user.color = 'orange';
-console.log(user);
+// const user = {};
+// user.name = 'Mango';
+// user.color = 'orange';
+// console.log(user);
 
 // for_in
 // for (key in object)
 
-let cat = {
-    name: 'Marcus',
-    color: 'orange',
-    age: 1.5,
-};
+// let cat = {
+//     name: 'Marcus',
+//     color: 'orange',
+//     age: 1.5,
+// };
 
-for (let key in cat) {
-    console.log(key);
-    console.log(cat[key]);
-};
+// for (let key in cat) {
+//     console.log(key);
+//     console.log(cat[key]);
+// };
 
-let codes = {
-    41: 'cat',
-    58: 'dog',
-    3: 'mango',
-    64: 'poly',
-    24: 'ben',
-};
+// let codes = {
+//     41: 'cat',
+//     58: 'dog',
+//     3: 'mango',
+//     64: 'poly',
+//     24: 'ben',
+// };
 
-for (let code in codes) {
-    console.log(code);
-};
+// for (let code in codes) {
+//     console.log(code);
+// };
 
