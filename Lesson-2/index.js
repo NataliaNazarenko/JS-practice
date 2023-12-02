748c2d022e0cfc1e726e732d86eeefdcd362e560
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2023-12-02 23:45:34 +0200

    add logical operators

--- Lesson-2/index.js
+++ Lesson-2/index.js
@@ -28,13 +28,79 @@
 // const newValue = Number(value);
 // console.log(typeof newValue);
 
-console.log(Boolean(1));
-console.log(Boolean(0));
-console.log(Boolean('name'));
-console.log(Boolean(''));
-console.log(Boolean(NaN));
-console.log(Boolean(null));
-console.log(Boolean(undefined));
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean('name'));
+// console.log(Boolean(''));
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+
+// унарний оператор, якщо він має один операнд
+
+// let x = 1;
+// x = -x;
+// console.log(x);
+
+// бінарний оператор, якщо він має два операнди
+
+// let c = 4, y = 2;
+// alert(c - y);
+
+// математичні операції
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+
+// залишок від ділення %
+
+// console.log(8 % 3);
+// console.log(10 % 3);
+
+// піднесення до степеня **
+
+// console.log(2 ** 3);
+// console.log(5 ** 2);
+
+// оператори порівняння
+
+// console.log(2 > 5);
+// console.log(2 < 5);
+// console.log(2 >= 10);
+// console.log(2 <= 4);
+
+// let data = "3";
+// console.log(data == 3); //не строге порівняння
+
+// let data = "3";
+// console.log(data === 3);  //строге порівняння
+
+// let data = "3";
+// console.log(data != 3); //не дорівнює
+
+// let data = "3";
+// console.log(data !== 3);  //строго не дорівнює
+
+// логічні оператори
+
+// and && оператор і
+
+// console.log(5 === 5 && 6 > 3);
+// console.log(5 === 6 && 6 > 3);
+// console.log(5 === 5 && 6 < 3);
+
+
+// or || оператор або
+
+console.log(5 + 5 === 25 || 2 + 2 === 6);
+console.log(5 * 5 === 25 || 2 * 2 === 6);
+console.log(5 + 5 === 25 || 2 * 2 === 6);
+console.log(5 * 5 === 25 || 2 + 2 === 6);
+
+
+
 
 
 
