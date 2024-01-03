b99b0d80db8c8fab0ae1f9937a257c29df8c88dc
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2024-01-03 22:07:43 +0200

    add bind()

--- Lesson-5/index.js
+++ Lesson-5/index.js
@@ -50,9 +50,71 @@
 
 // document.querySelector('p').onclick = abc;
 
-function changeColor() {
-    console.log(this);
-    this.style.backgroundColor = "blue";
-}
+// function changeColor() {
+//     console.log(this);
+//     this.style.backgroundColor = "blue";
+// };
 
-document.querySelector("div").onclick = changeColor;
+// document.querySelector("div").onclick = changeColor;
+
+// function changeColor() {
+//     this.style.backgroundColor = "green";
+// };
+
+// let user = document.querySelectorAll('div');
+// user.forEach(function(element) {
+//     element.onclick = changeColor;
+// });
+
+// стрілочна функція і this
+
+// const showList = () => {
+//     console.log(this);
+// };
+// showList();
+
+// let list = {
+//     names: ['Anna', 'Olga', 'Nata'],
+//     showList: showList,
+// };
+
+// list.showList(); // у стрілочної функції немає this
+
+// вбудовані методи bind
+
+// function hello() {
+//     console.log(this);
+// };
+
+// const user = {
+//     name: 'Mango',
+//     age: 30,
+//     hello: hello,
+//     sayHelloWindow: hello.bind(window),
+//     info: function(city) {
+//         console.log(`Name is ${this.name}`);
+//         console.log(`Age is ${this.age}`);
+//         console.log(`City is ${city}`);
+//     },
+// };
+
+// user.hello();
+// user.sayHelloWindow();
+// user.info();
+
+// const Apple = {
+//     name: 'Apple',
+//     age: 25,
+// };
+
+// const Kiwi = {
+//     name: 'Kiwi',
+//     age: 35,
+// };
+
+// user.info.bind(Apple)("Kyiv");
+// user.info.bind(Kiwi, "Odessa")();
+// const kiwiInfo = user.info.bind(Kiwi, "Odessa");
+// kiwiInfo();
+
+// вбудовані методи call
