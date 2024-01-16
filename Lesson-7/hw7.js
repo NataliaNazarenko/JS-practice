88308dcd8a3b606a9880eeb72348f758d9a090a4
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2024-01-16 21:09:52 +0200

    add file Lesson-8

--- Lesson-7/hw7.js
+++ Lesson-7/hw7.js
@@ -6,25 +6,25 @@
 // sum() повертає суму цих властивостей.
 // mul() повертає результат множення даних властивостей
 
-function Calculator() {
-    this.read = function() {
-        this.value1 = +prompt("Введіть перше значення");
-        this.value2 = +prompt("Введіть друге значення");
-    };
+// function Calculator() {
+//     this.read = function() {
+//         this.value1 = +prompt("Введіть перше значення");
+//         this.value2 = +prompt("Введіть друге значення");
+//     };
 
-    this.sum = function() {
-        return this.value1 + this.value2;
-    };
+//     this.sum = function() {
+//         return this.value1 + this.value2;
+//     };
 
-    this.mul = function() {
-        return this.value1 * this.value2;
-    };
-}
+//     this.mul = function() {
+//         return this.value1 * this.value2;
+//     };
+// }
 
-let calculator = new Calculator();
-calculator.read();
-let sumResult = calculator.sum();
-let mulResult = calculator.mul();
+// let calculator = new Calculator();
+// calculator.read();
+// let sumResult = calculator.sum();
+// let mulResult = calculator.mul();
 
-console.log("Сума: " + sumResult);
-console.log("Множення: " + mulResult);
+// console.log("Сума: " + sumResult);
+// console.log("Множення: " + mulResult);
