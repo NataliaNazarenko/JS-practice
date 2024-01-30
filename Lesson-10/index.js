7565bd2b6a5f4c3e910cd249374012b137016f74
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2024-01-30 21:24:46 +0200

    add setItem/getItem

--- Lesson-10/index.js
+++ Lesson-10/index.js
@@ -10,3 +10,28 @@ function check() {
         console.log('man');
     };
 };
+
+let form = document.querySelector('.form');
+let firstName = document.querySelector('.nameInput');
+let lastName = document.querySelector('.lastNameInput');
+let age = document.getElementById('age');
+let male = document.getElementsByName('radio');
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const userInfo = {
+        userName: firstName.value,
+        userLastName: lastName.value,
+        userAge: age.value,
+        userMale: male[0].checked,
+    };
+    console.log(userInfo);
+    localStorage.setItem('info', JSON.stringify(userInfo));
+};
+
+let data = localStorage.getItem('info');
+console.log(JSON.parse(data));
+
