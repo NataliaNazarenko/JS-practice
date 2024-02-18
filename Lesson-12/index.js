558395795790ca1f844fcd067592ced906776646
NataliaNazarenko <115888204+NataliaNazarenko@users.noreply.github.com>
2024-02-18 22:14:58 +0200

    add practic setTimeout

---
+++ Lesson-12/index.js
@@ -0,0 +1,18 @@
+//setTimeout
+setTimeout(() => {
+    // code to be executed after a certain delay
+}, 1000);
+
+console.log(1);
+setTimeout(() => {
+    console.log(2), 2000
+   
+})
+console.log(3);
+console.log(4);
+console.log(5);
+
+// setInterval
+setInterval(() => {
+    // code to be executed repeatedly
+})
